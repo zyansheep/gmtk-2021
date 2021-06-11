1ddb2baddf7c4b7b199677038d7618062bb026a3
zyansheep <zyansheep@github.com>
2021-06-11 14:53:30 -0400

    independant movement

---
+++ Main.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if Input.is_action_pressed("game_player_join"):
+		$"Player 1"
