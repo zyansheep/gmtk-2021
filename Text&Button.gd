27ed1b1dfb8cf4bbe9007e1d15facd5a5965d75a
OneTachi <52607373+OneTachi@users.noreply.github.com>
2021-06-12 10:48:39 -0400

    titlemenu update

--- Text&Button.gd
+++ Text&Button.gd
@@ -1,12 +1,30 @@
 extends RichTextLabel
 
 
+onready var timer = get_node("../SceneTimer")
 
+# true = start | false = exit
+var command = true
 
 func _on_StartButton_pressed():
 	fade_text_change()
-	#get_tree().change_scene("res://Main.tscn")
+	timer.start(.75)
+	command = true
 
+func _on_ExitButton_pressed():
+	fade_text_change()
+	timer.start(.75)
+	command = false
 
 func fade_text_change():
-	self.parse_bbcode = "[fade start=4 length=14][center]" + text + "[/center][/fade]"
+	self.parse_bbcode("[center][shake rate=10 level=20]" + text + "[/shake][/center]")
+
+
+func _on_SceneTimer_timeout():
+	if command:
+		get_tree().change_scene("res://Main.tscn")
+	else:
+		get_tree().quit()
+
+
+
