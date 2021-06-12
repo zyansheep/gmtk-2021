5ac1c26d215433d43c89573a2b430e394c2cdf20
zyansheep <zyansheep@github.com>
2021-06-12 10:07:44 -0400

    WIP camera movement

--- Camera2D.gd
+++ Camera2D.gd
@@ -5,21 +5,42 @@ extends Camera2D
 # var a = 2
 # var b = "text"
 
+var pathing = false;
+var baked_points;
+var current_point = -1;
+var zoom_target = Vector2(1,1)
+var zoom_lerp_speed = 0.5;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if $CameraPath:
+		if !$CameraPath.visible: return
+		pathing = true;
+		current_point = 0;
+		baked_points = $CameraPath.curve.get_baked_points();
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	zoom = lerp(zoom, zoom_target, zoom_lerp_speed);
+	if pathing:
+		zoom_target = Vector2(3.0,3.0);
+		if current_point < baked_points.size():
+			position = baked_points[current_point];
+			current_point += 2;
+		else:
+			pathing = false;
+		return
+	
 	var player_position_1 = $"../Player 1".position;
 	var player_position_2 = $"../Player 2".position;
 	
 	position = (player_position_1 + player_position_2) / 2;
+	var midpoint_dist = (player_position_1 - position)
 	var projectResolution=Vector2(ProjectSettings.get_setting("display/window/size/width"), ProjectSettings.get_setting("display/window/size/height"))
-	var test_zoom = ((player_position_1 - player_position_2) / projectResolution).length();
+	var test_zoom = (midpoint_dist * 2 / projectResolution).length();
 	var test_zoom_2 = max(test_zoom + 0.4, 1);
 	
-	zoom = Vector2(test_zoom_2, test_zoom_2);
+	zoom_target = Vector2(test_zoom_2, test_zoom_2);
 	pass
