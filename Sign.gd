d33f4e99a9256201da9db8d6d3005cd6593e2a91
zyansheep <zyansheep@github.com>
2021-06-12 09:48:09 -0400

    add sign

---
+++ Sign.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+export (String, MULTILINE) var Text = "C + ARROW\n TO DASH"
+export var Type_Speed = 0.1; #time delay between character prints
+
+onready var label = $CanvasLayer/Label
+
+func _ready():
+	label.text = Text;
+	$CanvasLayer.offset = position
+	$"Type Delay".wait_time = Type_Speed;
+	label.visible = false;
+
+func _on_Sign_body_entered(body):
+	if body.name.find("Player") == -1: return
+	print(body.name, " Entered")
+	label.visible_characters = 0
+	label.visible = true
+	$"Type Delay".start()
+
+
+func _on_Sign_body_exited(body):
+	if body.name.find("Player") == -1: return
+	label.visible = false
+	print("Body Exited")
+
+func _on_Type_Delay_timeout():
+	if label.visible_characters < label.text.length():
+		label.visible_characters += 1;
+		$"Type Delay".start();
+	else:
+		$"Type Delay".stop();
