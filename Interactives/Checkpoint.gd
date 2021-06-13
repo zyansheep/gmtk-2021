ba86049ce191685246ee34adbbe5d8962a13569d
zyansheep <zyansheep@github.com>
2021-06-13 10:48:03 -0400

    fix out of bounds trigerring on next level

--- Interactives/Checkpoint.gd
+++ Interactives/Checkpoint.gd
@@ -5,6 +5,7 @@ export (PackedScene) var next_level;
 func _on_DetectWin_body_entered(_body):
 	if next_level:
 		print("Next Level!")
+		$"../OutOfBounds".disabled = true;
 		get_tree().change_scene_to(next_level);
 	else:
 		print("No Level Found")
--- OutOfBounds.gd
+++ OutOfBounds.gd
@@ -1,4 +1,8 @@
 extends Area2D
 
+export(bool) var disabled = false;
+
 func _on_OutOfBounds_body_exited(body):
-	get_tree().reload_current_scene()
+	if body.name.find("Player") != -1 && !disabled:
+		print("Player went out of bounds!");
+		get_tree().reload_current_scene()
